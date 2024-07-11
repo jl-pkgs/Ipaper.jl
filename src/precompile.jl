ae1212362202733ad208a98885fb193932b7818f
Dongdong Kong <kongdd@users.noreply.github.com>
2024-07-12 01:43:52 +0800

    tidy precompile

--- src/precompile.jl
+++ src/precompile.jl
@@ -63,28 +63,5 @@ precompile(str_extract_strip, (String, String))
     end
 
     @pipe str |> _
-
-    ## precompile for cal_anomaly_quantile
-    function test_anomaly2(; T=Float32, dims=(2,))
-      ntime = length(dates)
-
-      A = rand(T, dims..., ntime)
-      kw = (; parallel=true, p1=1960, p2=1960, na_rm=false, probs=[0.5, 0.9])
-
-      @time anom_season = cal_anomaly_quantile(A, dates; kw..., method="season")
-      @time anom_base = cal_anomaly_quantile(A, dates; kw..., method="base")
-      # @time anom_full = cal_anomaly_quantile(A, dates; kw..., method="full")
-
-      @assert size(anom_base) == (dims..., ntime, length(kw.probs))
-      @assert size(anom_base) == size(anom_season)
-      # @assert size(anom_base) == size(anom_full)
-    end
-    # set_seed(1)
-    # l_dims = [(), (2,), (2, 2), (2, 2, 2)]
-    # for T in (Float32, Float64)
-    #   for dims = l_dims
-    #     test_anomaly2(; T, dims)
-    #   end
-    # end
   end
 end
