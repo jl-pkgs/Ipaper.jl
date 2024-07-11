fbb6b758fcd1f8eafd1ff627cff9df81a15eb7d5
Dongdong Kong <kongdd@users.noreply.github.com>
2024-07-11 21:19:05 +0800

    add License

--- ext/IpaperArchGDALExt/write_gdal.jl
+++ ext/IpaperArchGDALExt/write_gdal.jl
@@ -1,7 +1,10 @@
+## This part is borrowed from the GeoArrays.jl package.
+# MIT License, Copyright (c) 2018 Maarten Pronk
+# <https://github.com/evetion/GeoArrays.jl/blob/master/src/io.jl>
+
 # using GeoFormatTypes, ArchGDAL
 # WGS84 = convert(WellKnownText, EPSG(4326))
 # GFT.val(ga.crs)
-
 const WGS84 = "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AXIS[\"Latitude\",NORTH],AXIS[\"Longitude\",EAST],AUTHORITY[\"EPSG\",\"4326\"]]"
 
 # copied from `GeoArrays`
